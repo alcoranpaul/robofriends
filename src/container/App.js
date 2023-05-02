fa6fc20227b52a759eb3b29b38f0aad02d351168
Paul Adrian <paulreyes74@yahoo.com>
2023-05-01 22:16:45 -0500

    added react hooks

--- src/container/App.js
+++ src/container/App.js
@@ -1,48 +1,45 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css"
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ""
-        }
-    }
+function App() {
 
-    componentDidMount() {
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+
+    //Runs when function is called --- Similar to Beginplay in game engines
+    useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+            .then(users => setRobots(users));
+    }, []) //only run once
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+
+    if (robots.length === 0) {
+        return <h1>Loading...</h1>
+    }
+    else {
+        return (
+            <>
+                <div className="tc">
+                    <h1 className="f2">Robo Friends</h1>
+                    <SearchBox searchChange={onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            </>
+        )
     }
 
-    render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading...</h1>
-        }
-        else {
-            return (
-                <>
-                    <div className="tc">
-                        <h1 className="f2">Robo Friends</h1>
-                        <SearchBox searchChange={this.onSearchChange} />
-                        <CardList robots={filteredRobots} />
-                    </div>
-                </>
-            )
-        }
 
-    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import App from './App';
+import App from './container/App';
 import 'tachyons'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
